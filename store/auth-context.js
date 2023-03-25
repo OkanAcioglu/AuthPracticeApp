32a39c1dc57707a4896432ae7eea9ba0e57cc08f
Okan <okanacioglu@gmail.com>
2023-03-25 20:26:28 +0300

    logging-user-automatically

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,5 +1,6 @@
-import { createContext } from 'react'
-import { useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 export const AuthContext = createContext({
   token: '',
   isAuthenticated: false,
@@ -12,10 +13,12 @@ function AuthContextProvider({ children }) {
 
   function authenticate(token) {
     setAuthToken(token)
+    AsyncStorage.setItem('token', token)
   }
 
   function logout() {
     setAuthToken(null)
+    AsyncStorage.removeItem('token')
   }
 
   const value = {
